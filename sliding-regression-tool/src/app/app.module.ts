9dfd4ecdbbf228a587a8959597948d3ea3134611
Gia Jacob <mkaminski69@gmail.com>
2018-03-01 14:26:50 -0500

    Setup routing and added new components

--- sliding-regression-tool/src/app/app.module.ts
+++ sliding-regression-tool/src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {routing, appRoutingProviders} from "./app.routing";
 
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { TableComponent } from './table/table.component';
+import {RouterModule} from "@angular/router";
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    TableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
